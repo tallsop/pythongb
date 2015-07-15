65af971a151e1b2be322766a17ecc90af0cde379
Thomas Allsop <thomas.allsop11@gmail.com>
2015-07-15 15:23:25 +0100

    Added cycle count

---
+++ pythongb/gb.py
@@ -0,0 +1,12 @@
+from cpu import CPU
+
+
+class GameBoy(object):
+    def __init__(self):
+        # Perform launch operations
+
+        # Initialise the CPU
+        cpu = CPU()
+
+    def run(self):
+        pass
